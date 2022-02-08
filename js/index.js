6b3a01e8ccc7921bc26a2c991c81a29c07f6194a
Centeno Jose Antonio <jocecito182@gmail.com>
2022-02-08 13:16:22 -0300

    Finish app

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const agregarItemListener = document.getElementById('agregarItem').addEventListe
 const btnAgregar = document.getElementById('btnMostrarForm')
 const nuevaCompra = document.getElementById('nuevaCompra')
 const sinCompras = document.getElementById('sinCompras')
+const listaCompras = document.getElementById('listaCompras')
 
 let contadorId = 0
 
@@ -15,15 +16,34 @@ function mostrarForm() {
 
 function agregarItemList(e) {
     e.preventDefault()
-    let Id = contadorId
+    let id = contadorId
     let titulo = document.getElementById('titulo').value
     let icono = document.getElementById('icono').value
     let descripcion = document.getElementById('descripcion').value
     // validacion de titulo e icono
     if(titulo === '' || icono === ''){
-        alert('Debe ingresar un titulo de producto')
+        alert('Debe ingresar titulo y categoría de producto')
         return
     } else {
-
+        let newTitular = `<li class="listaCompraItem"><i class="${icono}"></i><p>${titulo}</p><i id="${id}" class="fas fa-chevron-down" onclick="showGladis(this.id)"></i><div class="descripciones off" id="div${id}"><i class="${icono}"></i><h3>${titulo}</h3><p>${descripcion}</p><i onclick="closeGladys(this.id)" id="close${id}" class="fas fa-times close"</i></div></li>`
+        listaCompras.innerHTML += newTitular
+        nuevaCompra.classList.add('off')
+        listaCompras.classList.remove('off')
+        console.log(listaCompras)
+        btnAgregar.classList.remove('off')
+        document.getElementById('titulo').value = ''
+        document.getElementById('icono').value = ''
+        document.getElementById('descripcion').value = ''
     }
+    contadorId++
+}
+
+function showGladis(id) {
+    let itemToShow = document.getElementById('div'+id)
+    itemToShow.classList.remove('off')
+}
+
+function closeGladys(id) {
+    let actualButton = document.getElementById(id);
+    actualButton.parentElement.classList.add('off')
 }
